cb030418d28fc2a5918d26931342d47d237cc29c
vmraj2 <vmraj2@sp26-cs525-1810.cs.illinois.edu>
2026-04-02 10:51:18 -0500

    Add metrics support

--- start_up_ow.sh
+++ start_up_ow.sh
@@ -7,7 +7,11 @@ source .env
 set +a
 
 # Rebuilds docker containers (maybe make this optional?)
-# ./gradlew distDocker
+./gradlew distDocker
+
+docker save whisk/invoker | ssh sp26-cs525-1819.cs.illinois.edu 'docker load'
+docker save whisk/invoker | ssh sp26-cs525-1818.cs.illinois.edu 'docker load'
+docker save whisk/invoker | ssh sp26-cs525-1817.cs.illinois.edu 'docker load'
 
 # Restarts from scratch, maybe make these steps optional?
 cd ansible
@@ -17,3 +21,9 @@ ansible-playbook -i environments/$ENVIRONMENT initdb.yml
 ansible-playbook -i environments/$ENVIRONMENT wipe.yml
 ansible-playbook -i environments/$ENVIRONMENT openwhisk.yml -e skip_pull_runtimes=true
 cd ..
+
+# run metrics service
+docker run -d --name user-events \
+  -p 9095:9095 \
+  -e KAFKA_HOSTS=172.17.0.1:9093 \
+  whisk/user-events:latest
